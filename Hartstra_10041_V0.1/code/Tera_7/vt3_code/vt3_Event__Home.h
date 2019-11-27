e5e0f1873ac71873cfdd247f135fc584e410431c
claire-ashley <40772983+claire-ashley@users.noreply.github.com>
2019-11-27 11:45:25 -0600

    AnimalControl-10041 V0.1
    animal control version 1

---
+++ Hartstra_10041_V0.1/code/Tera_7/vt3_code/vt3_Event__Home.h
@@ -0,0 +1,369 @@
+/* device:       /project/Tera_7 */
+/* device model: TERA7 */
+/* this file has been generated automatically by vt3 - do not modify! */
+
+
+#ifndef INCLUDE__VT3_EVENT__HOME_H
+#define INCLUDE__VT3_EVENT__HOME_H
+
+#include "vt3_base.h"
+
+/* timer event handler for screen: /project/Tera_7/Sreens/home */
+void vt3_event_ind_home(BYTE event)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Logo/multibitmap 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Logo_multibitmap(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F1/Button 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F1_Button(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F1/Text_1 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F1_Text_1(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F2/Button 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F2_Button(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F2/Text_1 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F2_Text_1(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F2/Auto_Man 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F2_Auto_Man(
+	BYTE event, 
+	vt3_indicator_MultiText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F3/Button 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F3_Button(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F3/Text_1 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F3_Text_1(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F4/Button 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F4_Button(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F4/Text_1 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F4_Text_1(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F5/Button 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F5_Button(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F5/Text_1 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F5_Text_1(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F6/Button 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F6_Button(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F6/Text_1 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F6_Text_1(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F9/Button 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F9_Button(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F9/Text_1 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F9_Text_1(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F10/Button 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F10_Button(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/F10/Text_1 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_F10_Text_1(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_1/multibitmap 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_1_multibitmap(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_1/Temp 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_1_Temp(
+	BYTE event, 
+	vt3_indicator_Number FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_1/multitext 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_1_multitext(
+	BYTE event, 
+	vt3_indicator_MultiText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_1/simple_text 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_1_simple_text(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_2/multibitmap 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_2_multibitmap(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_2/Temp 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_2_Temp(
+	BYTE event, 
+	vt3_indicator_Number FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_2/multitext 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_2_multitext(
+	BYTE event, 
+	vt3_indicator_MultiText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_2/simple_text 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_2_simple_text(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_3/multibitmap 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_3_multibitmap(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_3/Temp 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_3_Temp(
+	BYTE event, 
+	vt3_indicator_Number FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_3/multitext 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_3_multitext(
+	BYTE event, 
+	vt3_indicator_MultiText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_3/simple_text 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_3_simple_text(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_4/multibitmap 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_4_multibitmap(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_4/Temp 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_4_Temp(
+	BYTE event, 
+	vt3_indicator_Number FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_4/multitext 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_4_multitext(
+	BYTE event, 
+	vt3_indicator_MultiText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_4/simple_text 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_4_simple_text(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_5/multibitmap 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_5_multibitmap(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_5/Temp 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_5_Temp(
+	BYTE event, 
+	vt3_indicator_Number FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_5/multitext 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_5_multitext(
+	BYTE event, 
+	vt3_indicator_MultiText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_5/simple_text 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_5_simple_text(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_6/multibitmap 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_6_multibitmap(
+	BYTE event, 
+	vt3_indicator_MultiBitmap FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_6/Temp 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_6_Temp(
+	BYTE event, 
+	vt3_indicator_Number FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_6/multitext 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_6_multitext(
+	BYTE event, 
+	vt3_indicator_MultiText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/Kennel_Layout/Temp_6/simple_text 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_Kennel_Layout_Temp_6_simple_text(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/multitext 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_multitext(
+	BYTE event, 
+	vt3_indicator_MultiText FARPTR ind)
+;
+
+/* timer event handler for indicator: /project/Tera_7/Sreens/home/simple_text 
+ * runs every 100ms
+ */
+void vt3_event_ind_home_simple_text(
+	BYTE event, 
+	vt3_indicator_SimpleText FARPTR ind)
+;
+
+#endif /* INCLUDE__VT3_EVENT__HOME_H */
+
+/* end of file */
---
+++ Hartstra_10041_V0.1/code/Tera_7/vt3_code/vt3_signature.c
@@ -0,0 +1,16 @@
+/* device:       /project/Tera_7 */
+/* device model: TERA7 */
+/* vt3 version:  8.9.0.3-PATCH */
+/* this file has been generated automatically by vt3 - do not modify! */
+
+#include "vt3_runtime.h"
+
+
+/* the software signature */
+const unsigned char FAR vt3_signature_software[16] = {
+	0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00
+};
+
+
+
+/* end of file */
